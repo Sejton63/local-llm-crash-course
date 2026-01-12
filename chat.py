d7b304a6ddf0262805885f91eaf2aa0c0cbe24ac
Sejton63 <34143977+Sejton63@users.noreply.github.com>
2026-01-12 17:12:02 +0000

    add streaming and memory

---
+++ chat.py
@@ -0,0 +1,32 @@
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained(
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)
+
+
+def get_prompt(instruction: str, history: list[str] | None = None) -> str:
+    system = "You are an AI assistant that follows instruction extremely well. Help as much as you can. Give short answers."
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if history is not None:
+        prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
+    prompt += f"{instruction}\n\n### Response:\n"
+    print(f"Prompt created: {prompt}")
+    return prompt
+
+
+history = []
+question = "The capital of India is"
+prompt = get_prompt(question)
+answer = ""
+for word in llm(prompt, stream=True):
+    print(word, end="", flush=True)
+    answer += word
+print()
+history.append(answer)
+
+question = "And of the USA"
+prompt = get_prompt(question, history)
+for word in llm(prompt, stream=True):
+    print(word, end="", flush=True)
+print()
