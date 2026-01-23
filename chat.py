13c828ab80697fcb712f51132b34c62f9255155a
Sejton63 <34143977+Sejton63@users.noreply.github.com>
2026-01-23 14:20:30 +0000

    add chainlit

--- chat.py
+++ chat.py
@@ -1,4 +1,5 @@
 from ctransformers import AutoModelForCausalLM
+import chainlit as cl
 
 llm = AutoModelForCausalLM.from_pretrained(
     "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
@@ -15,6 +16,12 @@ def get_prompt(instruction: str, history: list[str] | None = None) -> str:
     return prompt
 
 
+@cl.on_message
+async def on_message(message: cl.Message):
+    response = f"Hello, you just sent: {message.content}!"
+    await cl.Message(response).send()
+
+"""
 history = []
 question = "The capital of India is"
 prompt = get_prompt(question)
@@ -30,3 +37,4 @@ prompt = get_prompt(question, history)
 for word in llm(prompt, stream=True):
     print(word, end="", flush=True)
 print()
+"""
