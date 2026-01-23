ea2b76d81ce29da895ba598ee8df0df6d003281a
Sejton63 <34143977+Sejton63@users.noreply.github.com>
2026-01-23 14:50:01 +0000

    add history

--- chat.py
+++ chat.py
@@ -5,26 +5,30 @@ import chainlit as cl
 def get_prompt(instruction: str, history: list[str] | None = None) -> str:
     system = "You are an AI assistant that follows instruction extremely well. Help as much as you can. Give short answers."
     prompt = f"### System:\n{system}\n\n### User:\n"
-    if history is not None:
+    if len(history) > 0:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
-    print(f"Prompt created: {prompt}")
     return prompt
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
+    message_history = cl.user_session.get("message_history")
     msg = cl.Message(content="")
     await msg.send()
 
-    prompt = get_prompt(message.content)
+    prompt = get_prompt(message.content, message_history)
+    response = ""
     for word in llm(prompt, stream=True):
         await msg.stream_token(word)
+        response += word
     await msg.update()
+    message_history.append(response)
 
 
 @cl.on_chat_start
 def on_chat_start():
+    cl.user_session.set("message_history", [])
     global llm
     llm = AutoModelForCausalLM.from_pretrained(
         "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
